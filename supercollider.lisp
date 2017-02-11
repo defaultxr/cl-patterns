3ceddde338a31301a820a561cbcf5c313d80328e
modula t. worm <defaultxr@gmail.com>
2017-02-10 19:40:04 -0600

    minor cleanup/fix to #'play-sc

--- supercollider.lisp
+++ supercollider.lisp
@@ -5,11 +5,10 @@
 (defmethod play-sc ((item t))
   (unless (eq (get-event-value item :type) :rest)
     (let ((params (copy-list (event-plist item)))
-          (time (+ (or (raw-get-event-value item :latency) 0.1) (or (raw-get-event-value item :unix-time-at-start) (sc:now))))
-          (node nil))
+          (time (+ (or (raw-get-event-value item :latency) 0.1) (or (raw-get-event-value item :unix-time-at-start) (sc:now)))))
       (remf params :instrument)
       (let ((node (sc:at time
-                    (setf node (sc:synth (instrument item) params)))))
+                    (sc:synth (instrument item) params))))
         (when (sc:has-gate-p node)
           (sc:at (+ time (dur-time (sustain item)))
             (sc:release node)))))))
