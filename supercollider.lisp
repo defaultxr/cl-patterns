3000b064cc27bcad3a8db1d982fb526de04701c1
modula t. worm <defaultxr@gmail.com>
2017-02-25 02:02:32 -0600

    prevent invalid events when no value exists for key.

--- supercollider.lisp
+++ supercollider.lisp
@@ -2,17 +2,21 @@
 
 (defgeneric play-sc (item))
 
-(defmethod play-sc ((item t))
+(defmethod play-sc ((item t)) ;; TODO: move the params stuff into its own function
   (unless (eq (get-event-value item :type) :rest)
     (let* ((inst (instrument item))
            (synth-params (getf sc::*synthdef-metadata* inst))
            (time (+ (or (raw-get-event-value item :latency) *latency*) (or (raw-get-event-value item :unix-time-at-start) (sc:now))))
            (params (if synth-params ;; make the parameters list for sc:synth.
                        (alexandria:flatten
-                        (loop :for sparam :in (mapcar #'car synth-params)
-                           :collect (cons sparam (if (eq :gate sparam)
-                                                     1
-                                                     (get-event-value item sparam)))))
+                        (remove-if #'null
+                                   (loop :for sparam :in (mapcar #'car synth-params)
+                                      :collect (let ((val (get-event-value item sparam)))
+                                                 (if val
+                                                     (cons sparam (if (eq :gate sparam)
+                                                                      1
+                                                                      val))
+                                                     nil)))))
                        (copy-list (event-plist item))))
            (params (mapcar (lambda (list-item) ;; replace buffers in the parameters list with their buffer numbers.
                              (if (typep list-item 'sc::buffer)
