1e00c0f0715db209080b29e955f7eb286b4b5396
modula t. worm <defaultxr@gmail.com>
2017-02-25 22:13:15 -0600

    function to get synth argument names

--- supercollider.lisp
+++ supercollider.lisp
@@ -1,22 +1,30 @@
 (in-package :cl-patterns)
 
+(defun get-synth-args-list (synth)
+  (or (mapcar #'car (getf sc::*synthdef-metadata* synth))
+      (mapcar #'alexandria:make-keyword
+              (remove-if (lambda (symbol)
+                           (or
+                            (position symbol (list '&key 'sc::end-f))
+                            (consp symbol)))
+                         (swank-backend:arglist (fdefinition (alexandria:ensure-symbol synth)))))))
+
 (defgeneric play-sc (item))
 
 (defmethod play-sc ((item t)) ;; TODO: move the params stuff into its own function
   (unless (eq (get-event-value item :type) :rest)
     (let* ((inst (instrument item))
-           (synth-params (getf sc::*synthdef-metadata* inst))
+           (synth-params (get-synth-args-list inst))
            (time (+ (or (raw-get-event-value item :latency) *latency*) (or (raw-get-event-value item :unix-time-at-start) (sc:now))))
            (params (if synth-params ;; make the parameters list for sc:synth.
                        (alexandria:flatten
                         (remove-if #'null
-                                   (loop :for sparam :in (mapcar #'car synth-params)
+                                   (loop :for sparam :in synth-params
                                       :collect (let ((val (get-event-value item sparam)))
-                                                 (if val
-                                                     (cons sparam (if (eq :gate sparam)
-                                                                      1
-                                                                      val))
-                                                     nil)))))
+                                                 (when val
+                                                   (cons sparam (if (eq :gate sparam)
+                                                                    1
+                                                                    val)))))))
                        (copy-list (event-plist item))))
            (params (mapcar (lambda (list-item) ;; replace buffers in the parameters list with their buffer numbers.
                              (if (typep list-item 'sc::buffer)
