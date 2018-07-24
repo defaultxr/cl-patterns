e4e08c14fd0657031795a678f10b1f678dbd4e39
modula t. worm <defaultxr@gmail.com>
2018-07-23 21:45:09 -0500

    add :pan support via CC; clamp values within acceptable ranges

--- src/backends/midi.lisp
+++ src/backends/midi.lisp
@@ -4,7 +4,7 @@
 
 (defparameter *midi-channels-instruments* (make-list 16 :initial-element 0))
 
-;; backend functions
+;;; backend functions
 
 (defun is-midi-event-p (event)
   (or (eq (event-value event :type) :midi)
@@ -12,12 +12,15 @@
 
 (defun play-midi (event &optional task)
   (declare (ignore task))
-  (let* ((channel (or (event-value event :channel)
-                      0))
-         (pgm (and (typep (event-value event :instrument) 'number)
-                   (truncate (event-value event :instrument))))
-         (note (truncate (event-value event :midinote)))
-         (velocity (round (* 127 (event-value event :amp)))) ;; FIX: maybe this shouldn't be linear
+  (let* ((channel (alexandria:clamp (or (event-value event :channel)
+                                        0)
+                                    0 15))
+         (pgm (alexandria:clamp (and (typep (event-value event :instrument) 'number)
+                                     (truncate (event-value event :instrument)))
+                                0 127))
+         (pan (alexandria:clamp (ceiling (* 63.5 (1+ (event-value event :pan)))) 0 127))
+         (note (alexandria:clamp (truncate (event-value event :midinote)) 0 127))
+         (velocity (alexandria:clamp (round (* 127 (event-value event :amp))) 0 127)) ;; FIX: maybe this shouldn't be linear?
          (time (local-time:timestamp+ (or (raw-event-value event :timestamp-at-start) (local-time:now))
                                       (truncate (* (or (raw-event-value event :latency) *latency*) 1000000000))
                                       :nsec)))
@@ -26,6 +29,7 @@
                       (when (and pgm
                                  (not (= pgm (nth channel *midi-channels-instruments*))))
                         (midihelper:send-event (midihelper:ev-pgmchange channel pgm)))
+                      (midihelper:send-event (midihelper:ev-cc channel 10 pan))
                       (midihelper:send-event (midihelper:ev-noteon channel note velocity))
                       (sleep (dur-time (sustain event)))
                       (midihelper:send-event (midihelper:ev-noteoff channel note velocity)))
