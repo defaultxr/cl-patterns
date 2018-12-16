3067790c742c403790b8bf0f4e866bc4e0834b48
modula t. worm <defaultxr@gmail.com>
2018-12-15 19:44:13 -0600

    rename event-special-slots to event-special-keys for consistency

--- src/event.lisp
+++ src/event.lisp
@@ -23,19 +23,19 @@
 (defvar *latency* 0.1
   "Default latency for events.")
 
-(defparameter *event-special-slots* (list)
-  "Plist mapping event special slots to their case lists.")
+(defparameter *event-special-keys* (list)
+  "Plist mapping event special keys to their case lists.")
 
-(defun event-value (event slot)
-  "Get the value of SLOT in EVENT, running any necessary conversion functions.
+(defun event-value (event key)
+  "Get the value of KEY in EVENT, running any necessary conversion functions.
 
-Returns 2 values: the value of the slot, and the name of the slot the value was derived from (or t if the default value of the slot was used, or nil if no value or default was provided)."
+Returns 2 values: the value of the key, and the name of the key the value was derived from (or t if the default value of the key was used, or nil if no value or default was provided)."
   (when (null event)
     (return-from event-value (values nil nil)))
-  (let* ((slot (alexandria:make-keyword slot))
-         (cases (car (getf *event-special-slots* slot)))
-         (cases (if (not (position slot (keys cases)))
-                    (append (list slot (lambda (event) (raw-event-value event slot))) cases)
+  (let* ((key (alexandria:make-keyword key))
+         (cases (car (getf *event-special-keys* key)))
+         (cases (if (not (position key (keys cases)))
+                    (append (list key (lambda (event) (raw-event-value event key))) cases)
                     cases))
          (cases-keys (keys cases))
          (key (car (or (member-if (lambda (k) (position k (keys event))) cases-keys)
@@ -60,52 +60,52 @@ Returns 2 values: the value of the slot, and the name of the slot the value was 
   (and (apply #'= (mapcar #'length lists))
        (apply #'every #'event-equal lists)))
 
-(defun (setf event-value) (value event slot)
-  "Set the value of SLOT to VALUE in EVENT, running any conversion functions that exist."
-  (let* ((slot (alexandria:make-keyword slot))
-         (cases (getf *event-special-slots* slot)))
+(defun (setf event-value) (value event key)
+  "Set the value of KEY to VALUE in EVENT, running any conversion functions that exist."
+  (let* ((key (alexandria:make-keyword key))
+         (cases (getf *event-special-keys* key)))
     (when (cadr cases) ;; remove-keys
       (let ((keys (remove-if (lambda (c) (eq c t)) (keys (car cases)))))
         (loop :for i :in keys
-           :do (remove-event-value event i))))
-    (raw-set-event-value event slot value)
+              :do (remove-event-value event i))))
+    (raw-set-event-value event key value)
     value))
 
-(defun raw-set-event-value (event slot value)
-  "Set the value of SLOT to VALUE in EVENT without running any conversion functions."
+(defun raw-set-event-value (event key value)
+  "Set the value of KEY to VALUE in EVENT without running any conversion functions."
   (with-slots (event-plist) event
-    (setf event-plist (plist-set event-plist (alexandria:make-keyword slot) value))))
+    (setf event-plist (plist-set event-plist (alexandria:make-keyword key) value))))
 
-(defun set-event-value (event slot value) ;; kind of deprecated. prefer (setf (event-value EVENT SLOT) VALUE) instead.
-  "Set the value of SLOT to VALUE in EVENT, running any conversion functions that exist.
+(defun set-event-value (event key value) ;; DEPRECATED; use (setf (event-value EVENT KEY) VALUE) instead.
+  "Set the value of KEY to VALUE in EVENT, running any conversion functions that exist.
 
-This function is semi-deprecated; use `(setf event-value)' instead, i.e.:
+This function is deprecated; use `(setf event-value)' instead, i.e.:
 
-;; (setf (event-value EVENT SLOT) VALUE)"
-  (setf (event-value event slot) value))
+;; (setf (event-value EVENT KEY) VALUE)"
+  (setf (event-value event key) value))
 
-(defun remove-event-value (event slot)
-  "Removes SLOT from EVENT."
+(defun remove-event-value (event key)
+  "Removes KEY from EVENT."
   (with-slots (event-plist) event
-    (setf event-plist (alexandria:remove-from-plist event-plist (alexandria:make-keyword slot))))
+    (setf event-plist (alexandria:remove-from-plist event-plist (alexandria:make-keyword key))))
   event)
 
-(defun raw-event-value (event slot)
-  "Get the value of SLOT in EVENT without running any conversion functions."
-  (getf (slot-value event 'event-plist) (alexandria:make-keyword slot)))
+(defun raw-event-value (event key)
+  "Get the value of KEY in EVENT without running any conversion functions."
+  (getf (slot-value event 'event-plist) (alexandria:make-keyword key)))
 
-(defun get-event-value (event &optional slot) ;; FIX: swap argument order, stop using this in cl-patterns
-  "Get the value of SLOT in EVENT, running any necessary conversion functions.
+(defun get-event-value (event &optional key) ;; FIX: swap argument order
+  "Get the value of KEY in EVENT, running any necessary conversion functions.
 
-The EVENT parameter can be omitted, in which case the SLOT slot is looked up from `*event*'.
+The EVENT parameter can be omitted, in which case the KEY key is looked up from `*event*'.
 
-Returns 2 values: the value of the slot, and the name of the slot the value was derived from (or t if the default value of the slot was used, or nil if no value or default was provided).
+Returns 2 values: the value of the key, and the name of the key the value was derived from (or t if the default value of the key was used, or nil if no value or default was provided).
 
 See also: `event-value'"
-  (when (null slot) ;; if called like (get-event-value :foo) then assume *event* is the event and :foo is the slot.
+  (when (null key) ;; if called like (get-event-value :foo) then assume *event* is the event and :foo is the key.
     (assert (typep event 'symbol))
     (return-from get-event-value (get-event-value *event* event)))
-  (event-value event slot))
+  (event-value event key))
 
 (defun combine-events (&rest events)
   "Returns an event that inserts all the items in each event of EVENTS. Keys from the events listed first will be overwritten by later events.
@@ -156,23 +156,23 @@ See also: `combine-events'"
 (defmethod print-object ((item event) stream)
   (format stream "(~s~{ ~s ~s~})" 'event (event-plist item)))
 
-(defmacro define-event-special-slot (name cases &key (remove-keys t) (define-methods nil))
-  "Define a special slot with the key NAME for events (i.e. slots that take their values from other slots, or slots that have default values).
+(defmacro define-event-special-key (name cases &key (remove-keys t) (define-methods nil))
+  "Define a special key with the key NAME for events (i.e. keys that take their values from other keys, or keys that have default values).
 
-CASES is a plist of cases mapping event slot names to forms. When `event-value' is called on an event for the NAME slot, then the event is tested for the keys of CASES in the order they're listed. The associated form of the first key of CASES that exists in the event is evaluated to get the value of that call to `event-value'.
+CASES is a plist of cases mapping event key names to forms. When `event-value' is called on an event for the NAME key, then the event is tested for the keys of CASES in the order they're listed. The associated form of the first key of CASES that exists in the event is evaluated to get the value of that call to `event-value'.
 
-If no case is provided with a KEY that's the same as NAME, one is automatically added at the start with a form that just returns the value of the event's NAME slot. If a case is provided with t as its KEY, that case is always run when found. This allows you to set a default value for the slot if none of the other keys from CASES exist in the event.
+If no case is provided with a KEY that's the same as NAME, one is automatically added at the start with a form that just returns the value of the event's NAME key. If a case is provided with t as its KEY, that case is always run when found. This allows you to set a default value for the key if none of the other keys from CASES exist in the event.
 
 REMOVE-KEYS is a list of keys to remove from the event when the NAME key is being set with `(setf event-value)'. If t (the default), all keys in CASES will be removed from the event.
 
-DEFINE-METHODS, if true, will cause the macro to define methods for getting and setting the slot in an event.
+DEFINE-METHODS, if true, will cause the macro to define methods for getting and setting the key in an event.
 
 Example:
-;; (define-event-special-slot amp (:db (db-amp (raw-event-value event :db))
+;; (define-event-special-key amp (:db (db-amp (raw-event-value event :db))
 ;;                                 t 0.5)
 ;;   :define-methods t)
 
-This defines the amp slot for events. Since the :amp KEY is implied, it doesn't need to be specified in the CASES. Thus if the event already has an :amp key, its value will be used by default. If no :amp key exists in the event, then the :db FUNC is run if the :db key exists. If neither :amp nor :db exist in the event, then the t key is run, giving a default of 0.5.
+This defines the amp key for events. Since the :amp KEY is implied, it doesn't need to be specified in the CASES. Thus if the event already has an :amp key, its value will be used by default. If no :amp key exists in the event, then the :db FUNC is run if the :db key exists. If neither :amp nor :db exist in the event, then the t key is run, giving a default of 0.5.
 
 Additionally, because :define-methods is true, we can also do the following:
 
@@ -184,8 +184,8 @@ Additionally, because :define-methods is true, we can also do the following:
     (unless (position kwname (keys cases))
       (setf cases (append (list kwname (list 'raw-event-value 'event kwname)) cases)))
     `(progn
-       (setf *event-special-slots*
-             (plist-set *event-special-slots* ,kwname (list
+       (setf *event-special-keys*
+             (plist-set *event-special-keys* ,kwname (list
                                                        (list ,@(loop :for (key value) :on cases :by #'cddr
                                                                   :append (list
                                                                            (if (eq key t)
@@ -205,106 +205,106 @@ Additionally, because :define-methods is true, we can also do the following:
 
 ;;; instrument/group/out
 
-(define-event-special-slot instrument (t :default)
+(define-event-special-key instrument (t :default)
   :define-methods t)
 
-(define-event-special-slot group (t 0))
+(define-event-special-key group (t 0))
 
-(define-event-special-slot out (t 0))
+(define-event-special-key out (t 0))
 
 ;;; amp/pan
 
-(define-event-special-slot amp (:db (db-amp (raw-event-value event :db))
+(define-event-special-key amp (:db (db-amp (raw-event-value event :db))
                                     t 0.5)
   :define-methods t)
 
-(define-event-special-slot db (:amp (amp-db (raw-event-value event :amp))
+(define-event-special-key db (:amp (amp-db (raw-event-value event :amp))
                                     t (amp-db 0.5))
   :define-methods t)
 
-(define-event-special-slot pan (t 0)
+(define-event-special-key pan (t 0)
   :define-methods t)
 
 ;;; dur/delta
 
-(define-event-special-slot tempo (t (if (and (boundp '*clock*) (not (null *clock*)))
+(define-event-special-key tempo (t (if (and (boundp '*clock*) (not (null *clock*)))
                                          (tempo *clock*)
                                          1)))
 
-(define-event-special-slot delta (:dur (event-value event :dur)
+(define-event-special-key delta (:dur (event-value event :dur)
                                        t (event-value event :dur))
   :remove-keys nil
   :define-methods t)
 
-(define-event-special-slot dur (:delta (raw-event-value event :delta)
-                                       t 1)
+(define-event-special-key dur (:delta (raw-event-value event :delta)
+                                      t 1)
   :remove-keys nil
   :define-methods t)
 
 ;; sustain/legato
 
-(define-event-special-slot sustain (t (* (event-value event :legato)
-                                         (event-value event :dur)))
+(define-event-special-key sustain (t (* (event-value event :legato)
+                                        (event-value event :dur)))
   :remove-keys (:legato)
   :define-methods t)
 
-(define-event-special-slot legato (:sustain (* (raw-event-value event :sustain)
-                                               (event-value event :dur))
-                                            t 0.8)
+(define-event-special-key legato (:sustain (* (raw-event-value event :sustain)
+                                              (event-value event :dur))
+                                           t 0.8)
   :remove-keys (:sustain)
   :define-methods t)
 
 ;;; timing-offset
 
-(define-event-special-slot timing-offset (t 0))
+(define-event-special-key timing-offset (t 0))
 
 ;;; quant
 
-(define-event-special-slot quant (t 1)
+(define-event-special-key quant (t 1)
   :define-methods t)
 
 ;;; freq/midinote/degree/octave/root/scale
 
-(define-event-special-slot freq (:midinote (midinote-freq (event-value event :midinote))
-                                           :degree (degree-freq (event-value event :degree)
-                                                                :root (event-value event :root)
-                                                                :octave (event-value event :octave)
-                                                                :scale (event-value event :scale))
-                                           t 440)
+(define-event-special-key freq (:midinote (midinote-freq (event-value event :midinote))
+                                :degree (degree-freq (event-value event :degree)
+                                                     :root (event-value event :root)
+                                                     :octave (event-value event :octave)
+                                                     :scale (event-value event :scale))
+                                          t 440)
   :remove-keys (:midinote :degree :root :octave)
   :define-methods t)
 
-(define-event-special-slot midinote (:freq (freq-midinote (event-value event :freq))
-                                           :degree (degree-midinote (event-value event :degree)
-                                                                    :root (event-value event :root)
-                                                                    :octave (event-value event :octave)
-                                                                    :scale (event-value event :scale))
-                                           t 69)
+(define-event-special-key midinote (:freq (freq-midinote (event-value event :freq))
+                                    :degree (degree-midinote (event-value event :degree)
+                                                             :root (event-value event :root)
+                                                             :octave (event-value event :octave)
+                                                             :scale (event-value event :scale))
+                                          t 69)
   :remove-keys (:freq :degree :root :octave)
   :define-methods t)
 
 ;; FIX: this can return NIL. i.e. (degree (event :midinote 0))
-(define-event-special-slot degree (:freq (midinote-degree (freq-midinote (event-value event :freq))
-                                                          :root (event-value event :root)
-                                                          :octave (event-value event :octave)
-                                                          :scale (event-value event :scale))
-                                         :midinote (midinote-degree (event-value event :midinote)
-                                                                    :root (event-value event :root)
-                                                                    :octave (event-value event :octave)
-                                                                    :scale (event-value event :scale))
-                                         t 5)
+(define-event-special-key degree (:freq (midinote-degree (freq-midinote (event-value event :freq))
+                                                         :root (event-value event :root)
+                                                         :octave (event-value event :octave)
+                                                         :scale (event-value event :scale))
+                                  :midinote (midinote-degree (event-value event :midinote)
+                                                             :root (event-value event :root)
+                                                             :octave (event-value event :octave)
+                                                             :scale (event-value event :scale))
+                                        t 5)
   :remove-keys (:freq :midinote)
   :define-methods t)
 
-(define-event-special-slot root (t 0) ;; FIX: can we derive this when :freq, :midinote, :degree, etc are available?
+(define-event-special-key root (t 0) ;; FIX: can we derive this when :freq, :midinote, :degree, etc are available?
   :remove-keys (:freq :midinote)
   :define-methods t)
 
-(define-event-special-slot octave (:freq (freq-octave (raw-event-value event :freq))
-                                         :midinote (midinote-octave (raw-event-value event :midinote))
-                                         t 5)
+(define-event-special-key octave (:freq (freq-octave (raw-event-value event :freq))
+                                  :midinote (midinote-octave (raw-event-value event :midinote))
+                                        t 5)
   :remove-keys (:freq :midinote)
   :define-methods t)
 
-(define-event-special-slot scale (t :major)
+(define-event-special-key scale (t :major)
   :remove-keys (:freq :midinote))
