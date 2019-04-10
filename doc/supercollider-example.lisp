99d9edef1cef476da7888bec8f53711542ddc785
modula t. worm <defaultxr@gmail.com>
2019-04-09 21:53:46 -0500

    major docs update
    - README
    - tutorial
    - supercollider-example
    - add patterns.org

--- doc/supercollider-example.lisp
+++ doc/supercollider-example.lisp
@@ -1,12 +1,20 @@
-(in-package :sc)
+;; to use cl-patterns with its SuperCollider backend, you'll first need to load the cl-patterns/supercollider system with quicklisp:
 
-;; start the sound server...
+(ql:quickload :cl-patterns/supercollider)
 
-(setf *s* (make-external-server "localhost" :port 4444))
+;; ...this will take care of loading cl-collider for you if it's not already loaded.
 
-(server-boot *s*)
+;; once that is done, start the SuperCollider server if you haven't already:
 
-;; define a few synths...
+(in-package :cl-patterns)
+
+(start-backend :supercollider)
+
+;; (Note: if you get an error, make sure that sc:*sc-synth-program* is the same as the output of "which scsynth" from your command line. You may also need to set sc:*sc-plugin-paths* if you get errors about UGens not being installed.)
+
+;; then define a few synths...
+
+(in-package :cl-collider)
 
 (defsynth kik ((freq 440) (out 0))
   (let* ((env (env-gen.kr (env (list 0 1 0) (list 0.001 1)) :act :free))
@@ -19,28 +27,38 @@
          (sig (sin-osc.ar freq 0 0.2)))
     (out.ar out (pan2.ar sig 0 env))))
 
+;; then, enable cl-patterns's supercollider backend:
+
 (in-package :cl-patterns)
 
-;; start the clock...
+(enable-backend :supercollider)
 
-;; the clock keeps tempo in beats per second; thus (/ 110 60) = 110 beats per minute
-(defparameter *clock* (make-clock (/ 110 60)))
+;; make a clock that patterns will be played on:
+
+;; the clock keeps tempo in beats per second; thus 110/60 = 110 beats per minute
+(defparameter *clock* (make-clock 110/60))
+
+;; run the clock in a new thread:
+
+(bt:make-thread (lambda () (clock-loop *clock*)) :name "cl-patterns clock-loop")
 
 ;; ...and then go ahead and write some patterns!
 
-(pdef :foo (pbind :instrument :kik ;; use the :kik synth
-                  :quant 4 ;; make sure the pattern will only start on a beat that is divisible by 4, to stay in sync
-                  :dur 1 ;; each event is 1 beat long
-                  :pfin 4 ;; limit the length of the pattern to 4 events (the default is infinite events)
-                  ))
+(pb :foo ;; define a new pattern named :foo
+  :instrument :kik ;; use the :kik synth we defined above
+  :quant 4 ;; make sure the pattern will only start on a beat that is divisible by 4, to stay in sync
+  :dur 1 ;; give each event a duration of 1 beat
+  :pfin 4 ;; limit the length of the pattern to 4 events (the default is infinite events)
+  )
 
-(pdef :bar (pbind :instrument :default
-                  :quant 4
-                  :dur 1/2
-                  :scale :major ;; select the major scale
-                  :degree (pwhite 0 7) ;; pick a random note from the first 7 notes in the selected scale
-                  :pfindur 4 ;; limit the length of the pattern to 4 beats (pfindur limits based on beats, not number of events)
-                  ))
+(pb :bar
+  :instrument :default
+  :quant 4
+  :dur 1/2
+  :scale :major ;; select the major scale
+  :degree (pwhite 0 7) ;; pick a random note from the first 7 notes in the selected scale
+  :pfindur 4 ;; limit the length of the pattern to 4 beats. pfindur causes the pattern to be limited based on its duration in beats, rather than the number of events.
+  )
 
 ;; start playing the defined patterns:
 
@@ -48,8 +66,23 @@
 
 (play :bar)
 
-;; stop playing
+;; a pattern will loop by default when played. so even though the above patterns have finite durations (as set with the :pfin and :pfindur keys), they will continue to play until you specifically stop them.
+;; while they're playing you can modify them by editing the code and re-evaluating it. the changes you make will be reflected the next time the patterns loops.
 
-(stop :foo)
+;; when you're done with them, you can stop playing the patterns like so:
 
-(stop :bar)
+(end :foo)
+
+(end :bar)
+
+;; the #'end function is used to make patterns stop at the end of their current loop, which is usually more musically desirable than stopping in the middle of the phrase.
+;; however, if you do want a pattern to stop immediately (or if you need it to because you defined it with an infinite length), you can use #'stop instead.
+
+;; to continue learning cl-patterns, you can use the documentation in the doc/ directory.
+;; in particular, tutorial.org is a tutorial meant for people new to cl-patterns.
+;; sc-differences.org may be useful if you're used to SuperCollider's patterns system.
+;; you can also introspect the library, for example to get a list of all defined pattern types:
+
+(all-patterns)
+
+;; then you can use Lisp's standard #'describe function to get information about any patterns defined.
