a5ee3b0042ab9323b42b33db57fffce8190936f6
modula t. worm <defaultxr@gmail.com>
2019-12-15 21:19:18 -0600

    bugfix for error when tasks are removed

--- src/clock.lisp
+++ src/clock.lisp
@@ -118,7 +118,7 @@ See also: `clock-add', `stop', `end'"
             (remove-if
              (lambda (ctask)
                (when-let ((eq (eq ctask task)))
-                 (dolist (backend (backends-for-event (slot-value task 'item)))
+                 (dolist (backend (enabled-backends))
                    (backend-task-removed task backend))
                  eq))
              tasks)))))
