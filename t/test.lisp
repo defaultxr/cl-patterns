0e65b3651cff6216241b13163052e465ad41058b
modula t. worm <defaultxr@gmail.com>
2019-12-27 13:40:29 -0600

    add test for undocumented exported symbols

--- t/test.lisp
+++ t/test.lisp
@@ -19,3 +19,19 @@
   (setf *previously-enabled-backends* (enabled-backends))
   (mapc 'disable-backend *previously-enabled-backends*)
   (enable-backend :debug))
+
+(defun undocumented-symbols (package)
+  "Get a list of all the undocumented external symbols in PACKAGE."
+  (let (symbols)
+    (do-external-symbols (sym package symbols)
+      (unless (position-if (lambda (type)
+                             (documentation sym type))
+                           (list 'function 'variable 'method-combination 'compiler-macro 'setf 'structure 'type))
+        (push sym symbols)))))
+
+(test undocumented-symbols
+  "Check for any undocumented exported symbols"
+  (let ((undocumented (undocumented-symbols :cl-patterns)))
+    (is-false undocumented
+              "some exported symbols do not have docstrings: ~s"
+              undocumented)))
