3bcedf094afb7490ac8e3a16e931fe658ce10a7a
modula t. worm <defaultxr@gmail.com>
2021-03-27 17:45:02 -0500

    alsa-midi: support :set event type and change CC keys to be :cc-
    previously, to send a control change message, you had to use a :c-
    key. now, more intuitively, they are :cc- instead.
    
    plus minor code cleanup

--- src/backends/alsa-midi.lisp
+++ src/backends/alsa-midi.lisp
@@ -6,11 +6,11 @@
 
 (defun alsa-midi-panic (&key channel manually-free)
   "Stop all notes on CHANNEL, or all channels if CHANNEL is nil."
-  (loop :for c :in (or (ensure-list channel) (list 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15))
-     :do (if manually-free
-             (loop :for note :from 0 :upto 127
-                :do (midihelper:send-event (midihelper:ev-noteoff c note 0)))
-             (midihelper:send-event (midihelper:ev-cc c 123 0)))))
+  (dolist (c (or (ensure-list channel) (iota 16)))
+    (if manually-free
+        (dolist (note (iota 128))
+          (midihelper:send-event (midihelper:ev-noteoff c note 0)))
+        (midihelper:send-event (midihelper:ev-cc c 123 0)))))
 
 ;;; cc mapping
 ;; http://nickfever.com/music/midi-cc-list
@@ -29,10 +29,10 @@
           cc-number)))
 
 (defun get-alsa-midi-auto-cc-mapping (key)
-  "Return auto-generated CC mapping parameters for a KEY of the format \"C-N\", where N is a valid CC number."
+  "Return auto-generated CC mapping parameters for a KEY of the format \"CC-N\", where N is a valid CC number."
   (let ((sym-name (symbol-name key)))
-    (when (and (>= (length sym-name) 2)
-               (string= "C-" (subseq sym-name 0 2)))
+    (when (and (>= (length sym-name) 3)
+               (string= "CC-" sym-name :end2 3))
       (let ((num (parse-integer (subseq sym-name 2) :junk-allowed t)))
         (when (and (integerp num)
                    (<= 0 num 127))
@@ -45,6 +45,16 @@
     (if (numberp key-map)
         (gethash key-map *alsa-midi-cc-table*)
         key-map)))
+
+(defun midi-remap-key-value (key value)
+  "Remap KEY and VALUE to their equivalent MIDI CC parameter and range. Returns a list of the format (CC-NUMBER CC-VALUE), or nil if the key is not a recognized CC key."
+  (if-let ((cc-mapping (get-alsa-midi-cc-mapping key)))
+    (list (car cc-mapping) (funcall (nth 3 cc-mapping) value))
+    (let ((sym-name (symbol-name key)))
+      (if (and (>= (length sym-name) 3)
+               (string= "CC-" sym-name :end2 3))
+          (list (midi-truncate-clamp (parse-integer (subseq sym-name 3))) (midi-truncate-clamp value))
+          nil))))
 
 ;; set default cc mappings
 (mapc (lambda (x) (apply 'set-alsa-midi-cc-mapping x))
@@ -95,32 +105,32 @@
   nil)
 
 (defmethod backend-play-event (event task (backend (eql :alsa-midi)))
-  (when (or (eql (event-value event :type) :midi)
-            (typep (event-value event :instrument) 'number))
-    (let* ((channel (midi-truncate-clamp (or (event-value event :channel) 0) 15))
-           (pgm (midi-truncate-clamp (multiple-value-bind (value from) (event-value event :instrument)
-                                       (if (eql t from)
-                                           0
-                                           (if (integerp value) value 0))))) ;; FIX: provide an instrument translation table to automatically translate instrument names to program numbers
-           (note (midi-truncate-clamp (event-value event :midinote)))
-           (velocity (unipolar-1-to-midi (event-value event :amp))) ;; FIX: maybe this shouldn't be linear?
-           (time (or (raw-event-value event :timestamp-at-start) (local-time:now)))
-           (extra-params (loop :for key :in (keys event)
-                               :for cc-mapping := (get-alsa-midi-cc-mapping key)
-                               :if cc-mapping
-                                 :collect (list (car cc-mapping) (funcall (nth 3 cc-mapping) (event-value event key))))))
-      (bt:make-thread
-       (lambda ()
-         (sleep (max 0 (local-time:timestamp-difference time (local-time:now))))
-         (when (and pgm
-                    (not (= pgm (nth channel *alsa-midi-channels-instruments*))))
-           (midihelper:send-event (midihelper:ev-pgmchange channel pgm)))
-         (dolist (param extra-params)
-           (midihelper:send-event (midihelper:ev-cc channel (car param) (cadr param))))
+  (let* ((type (event-value event :type))
+         (channel (midi-truncate-clamp (or (event-value event :channel) 0) 15))
+         (pgm (midi-truncate-clamp (multiple-value-bind (value from) (event-value event :instrument)
+                                     (if (eql t from)
+                                         0
+                                         (if (integerp value) value 0))))) ;; FIX: provide an instrument translation table to automatically translate instrument names to program numbers
+         (note (midi-truncate-clamp (event-value event :midinote)))
+         (velocity (unipolar-1-to-midi (event-value event :amp))) ;; FIX: maybe this shouldn't be linear?
+         (time (or (raw-event-value event :timestamp-at-start) (local-time:now)))
+         (extra-params (loop :for key :in (keys event)
+                             :for cc-mapping := (midi-remap-key-value key (event-value event key))
+                             :if cc-mapping
+                               :collect cc-mapping)))
+    (bt:make-thread
+     (lambda ()
+       (sleep (max 0 (local-time:timestamp-difference time (local-time:now))))
+       (when (and pgm
+                  (not (= pgm (nth channel *alsa-midi-channels-instruments*))))
+         (midihelper:send-event (midihelper:ev-pgmchange channel pgm)))
+       (dolist (param extra-params)
+         (midihelper:send-event (midihelper:ev-cc channel (car param) (cadr param))))
+       (unless (eql type :set)
          (midihelper:send-event (midihelper:ev-noteon channel note velocity))
          (sleep (max 0 (dur-time (sustain event) (tempo (slot-value task 'clock)))))
-         (midihelper:send-event (midihelper:ev-noteoff channel note velocity)))
-       :name "cl-patterns temporary alsa midi note thread"))))
+         (midihelper:send-event (midihelper:ev-noteoff channel note velocity))))
+     :name "cl-patterns temporary alsa midi note thread")))
 
 (defmethod backend-task-removed (task (backend (eql :alsa-midi)))
   ;; FIX
