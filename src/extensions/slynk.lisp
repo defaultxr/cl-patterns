ddaba1977a9362aa58a9bd0c8afe0242c1a94354
modula t. worm <defaultxr@gmail.com>
2021-08-10 21:20:45 -0500

    improvement/bugfix for pnary swank/slynk computed arglist

--- src/extensions/slynk.lisp
+++ src/extensions/slynk.lisp
@@ -2,4 +2,14 @@
 
 ;; show the arguments of the function being called in pnary
 (defmethod slynk::compute-enriched-decoded-arglist ((operator-form (eql 'pnary)) argument-forms)
-  (slynk::compute-enriched-decoded-arglist 'apply argument-forms))
+  (let ((function-name-form (car argument-forms)))
+    (when (and (listp function-name-form)
+               (length= 2 function-name-form)
+               (member (car function-name-form) '(quote function) :test #'eq))
+      (let ((function-name (cadr function-name-form)))
+        (when (fboundp function-name)
+          (let ((function-arglist (slynk::compute-enriched-decoded-arglist function-name (cdr argument-forms))))
+            (push 'operator (slynk::arglist.required-args function-arglist))
+            (return-from slynk::compute-enriched-decoded-arglist
+              (values function-arglist (list function-name-form) t)))))))
+  (call-next-method))
--- src/extensions/swank.lisp
+++ src/extensions/swank.lisp
@@ -2,4 +2,14 @@
 
 ;; show the arguments of the function being called in pnary
 (defmethod swank::compute-enriched-decoded-arglist ((operator-form (eql 'pnary)) argument-forms)
-  (swank::compute-enriched-decoded-arglist 'apply argument-forms))
+  (let ((function-name-form (car argument-forms)))
+    (when (and (listp function-name-form)
+               (length= 2 function-name-form)
+               (member (car function-name-form) '(quote function) :test #'eq))
+      (let ((function-name (cadr function-name-form)))
+        (when (fboundp function-name)
+          (let ((function-arglist (swank::compute-enriched-decoded-arglist function-name (cdr argument-forms))))
+            (push 'operator (swank::arglist.required-args function-arglist))
+            (return-from swank::compute-enriched-decoded-arglist
+              (values function-arglist (list function-name-form) t)))))))
+  (call-next-method))
