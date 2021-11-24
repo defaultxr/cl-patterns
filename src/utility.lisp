defaultxr/cl-patterns

commit 0effeb14feae31491f366c20939e6bd7c0732790

improve and add tests for find-object-by-id

src/utility.lisp
@@ -340,14 +340,13 @@ Example:
 See also: `play', `launch', `end', `stop'")
 
 (defun find-object-by-id (id &key default)
-  "Find an object identified by ID using `*dictionary-lookup-functions*'. Returns DEFAULT if no object was found. If DEFAULT is a symbol with name \"error\" then throw an error.
+  "Find an object identified by ID using `*dictionary-lookup-functions*'. Returns DEFAULT if no object was found, or signals an error if DEFAULT is the symbol :error.
 
 See also: `find-pdef'"
-  (dolist (func *dictionary-lookup-functions* (if (string= 'error default)
-                                                  (error "No object found with ID ~s" id)
-                                                  default))
-    (when-let ((res (ignore-errors (funcall func id))))
-      (return-from find-object-by-id res))))
+  (or (find-if (fn (ignore-errors (funcall _ id))) *dictionary-lookup-functions*)
+      (if (eql :error default)
+          (error "No object found with ID ~s" id)
+          default)))
 
 ;;; generics
 

t/utility.lisp
@@ -131,6 +131,17 @@
   ;; FIX: test more of the negative DIRECTION
   )
 
+(test find-object-by-id
+  "Test the `find-object-by-id' function"
+  (with-fixture temporary-pdef-dictionary ()
+    (is-false (cl-patterns::find-object-by-id '#:does-not-exist :default nil)
+              "find-object-by-id doesn't return nil for undefined keys")
+    (signals (simple-error "find-object-by-id doesn't return raise an error for undefined keys when :default is :error")
+      (cl-patterns::find-object-by-id '#:does-not-exist :default :error))
+    (pdef :foo (pn 1 1))
+    (is-true (cl-patterns::find-object-by-id :foo)
+             "find-object-by-id doesn't return true for defined keys")))
+
 (test tempo
   "Test the `tempo' function"
   ;; FIX

commit beff167106081af93ea12ce6e797473c023c92e2

fix find-object-by-id and add a test for this fix
fixes the bug introduced in commit 0effeb14feae31491f366c20939e6bd7c0732790

src/utility.lisp
@@ -343,10 +343,11 @@ See also: `play', `launch', `end', `stop'")
   "Find an object identified by ID using `*dictionary-lookup-functions*'. Returns DEFAULT if no object was found, or signals an error if DEFAULT is the symbol :error.
 
 See also: `find-pdef'"
-  (or (find-if (fn (ignore-errors (funcall _ id))) *dictionary-lookup-functions*)
-      (if (eql :error default)
-          (error "No object found with ID ~s" id)
-          default)))
+  (dolist (func *dictionary-lookup-functions* (if (eql :error default)
+                                                  (error "No object found with ID ~s" id)
+                                                  default))
+    (when-let ((res (ignore-errors (funcall func id))))
+      (return-from find-object-by-id res))))
 
 ;;; generics
 

t/utility.lisp
@@ -140,7 +140,11 @@
       (cl-patterns::find-object-by-id '#:does-not-exist :default :error))
     (pdef :foo (pn 1 1))
     (is-true (cl-patterns::find-object-by-id :foo)
-             "find-object-by-id doesn't return true for defined keys")))
+             "find-object-by-id doesn't return true for defined keys")
+    (let ((cl-patterns::*dictionary-lookup-functions* (list 'find-pdef)))
+      (is (eq (find-pdef :foo)
+              (cl-patterns::find-object-by-id :foo))
+          "find-object-by-id doesn't return the object itself when one is defined"))))
 
 (test tempo
   "Test the `tempo' function"