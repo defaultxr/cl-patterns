a741bf6d35e209107e4513b32a42466198aeecfb
modula t. worm <defaultxr@gmail.com>
2022-05-12 18:58:18 -0500

    use mutility:approx= for some tests that suffer from rounding error

--- t/conversions.lisp
+++ t/conversions.lisp
@@ -8,16 +8,15 @@
 ;; FIX: add more.
 
 ;; NOTE: Many of the conversion functions are affected by floating point rounding errors.
-;; This is why only some numbers are tested for db-amp, freq-midinote, etc.
-;; The point here is mainly to guard against regressions, not to ensure that all functions have mathematically correct results.
-;; Because as far as I know, it's not possible--at least in SBCL--to get more accurate results than what we have currently.
+;; This is why we use `mutility:approx=' for many of these comparisons rather than `cl:='.
 
 (test amplitude-conversion
   "Test amplitude conversion functions"
-  (is (equal
-       (list 0.1 0.5 0.7 0.8 0.9 1.0)
-       (mapcar #'db-amp (mapcar #'amp-db (list 0.1 0.5 0.7 0.8 0.9 1.0))))
-      "db-amp conversion is not equivalent to amp-db conversion"))
+  (is-true (let ((input (iota 10 :start 1/10 :step 1/10)))
+             (every #'identity (mapcar #'approx=
+                                       (mapcar #'db-amp (mapcar #'amp-db input))
+                                       input)))
+           "db-amp conversion is not equivalent to amp-db conversion"))
 
 (test time-conversion
   "Test time conversion functions"
@@ -27,18 +26,18 @@
 
 (test pitch-conversion
   "Test pitch conversion functions"
-  (is-false (let ((input (remove 20 (iota 128)))) ;; 20 seems to be the only input that has rounding errors...
-              (position nil (mapcar #'= input (mapcar #'freq-midinote (mapcar #'midinote-freq input)))))
+  (is-false (let ((input (iota 128)))
+              (position nil (mapcar #'approx=
+                                    input
+                                    (mapcar #'freq-midinote (mapcar #'midinote-freq input)))))
             "freq-midinote conversion is not equivalent to midinote-freq conversion")
-  (is (every #'identity
-             (mapcar #'=
-                     (list 13.75 27.5 55 110 220 440 880 3520)
-                     (mapcar #'midinote-freq (list 9 21 33 45 57 69 81 105))))
+  (is (every #'identity (mapcar #'=
+                                (list 13.75 27.5 55 110 220 440 880 3520)
+                                (mapcar #'midinote-freq (list 9 21 33 45 57 69 81 105))))
       "midinote-freq conversion is not correct")
-  (is (equal
-       (loop :for i :from 0 :upto 10
-             :append (make-list 12 :initial-element i))
-       (mapcar #'midinote-octave (iota 132)))
+  (is (equal (loop :for i :from 0 :upto 10
+                   :append (make-list 12 :initial-element i))
+             (mapcar #'midinote-octave (iota 132)))
       "midinote-octave conversion is not correct")
   (is (= (degree-freq 0)
          (midinote-freq (degree-midinote 0)))
