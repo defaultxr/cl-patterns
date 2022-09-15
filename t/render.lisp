042d48fc9268468fae2e682fbd3163274674ce43
modula t. worm <defaultxr@gmail.com>
2022-09-14 23:22:43 -0500

    add another `render` test

--- t/render.lisp
+++ t/render.lisp
@@ -13,5 +13,6 @@
   (is (typep (render (pseq (list 1 2 3)) :list) 'list)
       "render :list does not coerce to a list")
   (is (length= 7 (render (pseq (list 1 2)) :list :max-length 7))
-      "render :list with :max-length 7 renders the wrong number of items"))
-
+      "render :list with :max-length 7 renders the wrong number of items")
+  (is (= 3 (reduce #'+ (mapcar #'dur (render (pbind :dur (pn 1 3)) :list :max-duration 8))))
+      "render :list with :max-duration 8 on a 3-dur pattern does not result in 3-dur output"))
