a70493ccb071ad3636d664bf4855ae215a4d808d
modula t. worm <defaultxr@gmail.com>
2023-01-18 00:18:13 -0600

    debug backend: fix events list (it wasn't keeping all events before)
    also minor refactor

--- src/backends/debug.lisp
+++ src/backends/debug.lisp
@@ -1,11 +1,15 @@
-;;;; debug.lisp - the debug backend for cl-patterns; simply prints incoming events for debugging purposes.
+;;;; debug.lisp - the debug backend for cl-patterns.
+;;; simply prints and stores incoming events for debugging purposes.
 
 (in-package #:cl-patterns)
 
 (defclass debug-backend (backend)
   ((name :initform "debug")
    (print-stream :initarg :print-stream :initform nil :accessor debug-backend-print-stream :documentation "The stream to print debugging output to, or nil if no printing should be done.")
-   (recent-events :initform (list) :writer (setf debug-backend-recent-events) :type list :documentation "List of events received by the backend, with the most recent events first.")))
+   (events :initform (list) :accessor debug-backend-events :type list :documentation "List of events received by the backend, with the most recent events first.")))
+
+(setf (documentation 'debug-backend-print-stream 'function) "The stream to print debugging output to, or nil if no printing should be done."
+      (documentation 'debug-backend-events 'function) "List of events received by the backend, with the most recent events first.")
 
 (defmethod backend-start ((backend debug-backend) &key)
   (format (debug-backend-print-stream backend) "~&Starting debug backend (i.e. doing nothing).~%"))
@@ -13,24 +17,27 @@
 (defmethod backend-stop ((backend debug-backend))
   (format (debug-backend-print-stream backend) "~&Stopping debug backend (i.e. doing nothing).~%"))
 
+(defun (setf debug-backend-recent-events) (value &optional (backend (find-backend 'debug-backend)))
+  (setf (debug-backend-events backend) value))
+
 (defun debug-backend-recent-events (&optional (backend (find-backend 'debug-backend)) (n 10))
   "Get the N most recent events recorded to the debug backend (most recent first). You will of course need to enable the debug backend first in order to record events with it.
 
 See also: `ptrace'"
-  (loop :for i :in (slot-value backend 'recent-events)
-        :repeat n
+  (loop :repeat n
+        :for i :in (debug-backend-events backend)
         :collect i))
 
 (defun debug-backend-clear-recent-events (&optional (backend (find-backend 'debug-backend)))
   "Clear the log of events captured by a `debug-backend'.
 
 See also: `debug-backend-recent-events'"
-  (setf (slot-value backend 'recent-events) (list)))
+  (setf (debug-backend-events backend) nil))
 
 (defmethod backend-play-event ((backend debug-backend) event task)
   (declare (ignore task))
   (format (debug-backend-print-stream backend) "~&Debug: playing event: ~S~%" event)
-  (push event (debug-backend-recent-events backend)))
+  (push event (debug-backend-events backend)))
 
 (defmethod backend-task-removed ((backend debug-backend) task)
   (format (debug-backend-print-stream backend) "~&Debug: task removed: ~S~%" task))
@@ -38,7 +45,7 @@ See also: `debug-backend-recent-events'"
 (defmethod backend-timestamps-for-event ((backend debug-backend) event task)
   nil)
 
-(export '(debug-backend-recent-events debug-backend-clear-recent-events debug-recent-events debug-clear-events *debug-print-events*))
+(export '(debug-backend-print-stream debug-backend-events debug-backend-recent-events debug-backend-clear-recent-events debug-recent-events debug-clear-events *debug-print-events*))
 
 ;;; deprecated
 
